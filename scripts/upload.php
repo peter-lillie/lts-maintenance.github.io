16e665ca63d2d21e62ca9087857fdd472707f0b6
Peter Lillie <sealchest@gmail.com>
2022-12-11 21:43:45 -0500

    Got the DB working probably?

--- scripts/upload.php
+++ scripts/upload.php
@@ -1,16 +1,37 @@
 <?php
 
+include 'dbconfig.php';
+$statusMsg = "";
+
 /* Get the name of the uploaded file */
-$filename = $_FILES['file']['name'];
+$fileName = $_FILES['file']['name'];
+$targetDir = "../images/uploads/";
+$targetFilePath = $targetDir.$fileName;
+$fileType = pathinfo($targetFilePath,PATHINFO_EXTENSION);
 
-/* Choose where to save the uploaded file */
-$location = "../images/uploads/".$filename;
-
-/* Save the uploaded file to the local filesystem */
-if ( move_uploaded_file($_FILES['file']['tmp_name'], $location) ) { 
-  echo 'Success'; 
-} else { 
-  echo 'Failure'; 
+if(isset($_POST["submit"]) && !empty($_FILES["file"]["name"])){
+  // Allow certain file formats
+  $allowTypes = array('jpg','png','jpeg','gif','pdf');
+  if(in_array($fileType, $allowTypes)){
+      // Upload file to server
+      if(move_uploaded_file($_FILES["file"]["tmp_name"], $targetFilePath)){
+          // Insert image file name into database
+          $insert = $db->query("INSERT into photouploads (uploaddate, photoname) VALUES (NOW(), '".$fileName."')");
+          if($insert){
+              $statusMsg = "The file ".$fileName. " has been uploaded successfully.";
+          }else{
+              $statusMsg = "File upload failed, please try again.";
+          } 
+      }else{
+          $statusMsg = "Sorry, there was an error uploading your file.";
+      }
+  }else{
+      $statusMsg = 'Sorry, only JPG, JPEG, PNG, GIF, & PDF files are allowed to upload.';
+  }
+}else{
+  $statusMsg = 'Please select a file to upload.';
 }
 
+// Display status message
+echo $statusMsg;
 ?>
